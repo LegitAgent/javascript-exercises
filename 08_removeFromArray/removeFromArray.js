21ac4dd734b8863eafc05c2602d4d9ae43840a80
Alba Martin <albamartindarius@gmail.com>
2025-10-18 15:52:30 +0800

    answers

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+    const hashset = new Set(numbers);
+    return array.filter(num => !hashset.has(num));
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+    if (x < 0 || y < 0) return "ERROR";
+    let start;
+    let finish;
+    if(x > y) {
+        start = y;
+        finish = x;
+    } else {
+        start = x;
+        finish = y;
+    }
+    let sum = 0;
+    for(let i = start; i <= finish; i++) {
+        sum += i;
+    }
+    return sum;
 };
+console.log(sumAll(123, 1))
 
 // Do not edit below this line
 module.exports = sumAll;
